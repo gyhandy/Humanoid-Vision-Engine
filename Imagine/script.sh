5cf6cdf710c42f37f781b4ea9796403cd184dda1
Xingrui Wang <XingrWang@gmail.com>
2022-07-17 22:43:36 -0700

    Update script.sh

--- Imagine/script.sh
+++ Imagine/script.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-python main.py --cuda 0,1 --batch_size 16 --resume_epoch 0
+FEATURE=texture
+
+python main.py --cuda 0,1 \
+               --batch_size 16 \
+               --dataset_path /lab/tmpig8d/u/yao_data/human_simulation_engine/V3_${FEATURE}_dataset \
+               --output_path out/deeper/${FEATURE}
